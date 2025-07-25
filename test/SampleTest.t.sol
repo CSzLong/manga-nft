ec8ece7519c5952217abede54ba83ab4f1e893db
CSzlong <szlong1987@gmail.com>
2025-07-25 20:45:03 +0800

    testing

--- test/SampleTest.t.sol
+++ test/SampleTest.t.sol
@@ -1,10 +1,11 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
 
 import "forge-std/Test.sol";
 
 contract SampleTest is Test {
     function testExample() public {
-        assertEq(1, 1);
+        assert(true);
     }
 }
